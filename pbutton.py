6f2fc037e308041148ae3c3b19bce6294bf39080
torstenboe <torsten.boettjer@gmail.com>
2017-04-08 20:27:33 +0200

    Correct Input Template

--- pbutton.py
+++ pbutton.py
@@ -2,12 +2,12 @@ import  RPi.GPIO as GPIO
 import time
 
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
 
 while True:
-    inputValue = GPIO.input(18)
+    inputValue = GPIO.input(12)
     if (inputValue == False):
         print("Button press ")
-    #time.sleep(0.3)
+    time.sleep(0.3)
