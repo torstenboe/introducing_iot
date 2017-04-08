4e7cb855e86545d72c1fd81f708f9fd7d3e95d0d
torstenboe <torsten.boettjer@gmail.com>
2017-04-08 20:51:49 +0200

    Correct Input Template

--- pushblink.py
+++ pushblink.py
@@ -4,17 +4,18 @@ import time
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT)
-GPIO.setup(12, GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(12, GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
 
 while True:
     inputValue = GPIO.input(12)
-    if (inputValue == True):
+    if (inputValue == False):
         GPIO.output(11, True)
+        print("Button press ")
     else:
         GPIO.output(11, True)
         time.sleep(1)
         GPIO.output(11, False)
         time.sleep(1)
-        print("Button press ")
+        print("Button not press ")
     time.sleep(0.3)
