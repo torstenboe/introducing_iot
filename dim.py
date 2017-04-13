f3b380a453cc24fcdcbb988d80432bf13ad0bf68
torstenboe <torsten.boettjer@gmail.com>
2017-04-13 13:58:11 +0200

    Correct Input Template

--- dim.py
+++ dim.py
@@ -7,10 +7,10 @@ GPIO.setup(11, GPIO.OUT)
 pwm = GPIO.PWM(11, 50)
 pwm.start(0)
 
-foreach i in range(100):
+for i in range(100):
    pwm.ChangeDutyCycle(i)
    time.sleep(0.1)
-foreach i in range(100, 0, -1)
+for i in range(100, 0, -1)
    pwm.ChangeDutyCycle(i)
    time.sleep(0.1)
 
