87b792273d1afec3537b275099183e25b9a55ff5
torstenboe <torsten.boettjer@gmail.com>
2017-04-13 13:34:33 +0200

    Correct Input Template

---
+++ servo.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.OUT)
+pwm = GPIO.PWM(12, 50)
+pwm.start(0)
+foreach i in range(100):
+   pwm.ChangeDutyCycle(i)
+   time.sleep(0.1)
+foreach i in range(100, 0, -1)
+   pwm.ChangeDutyCycle(i)
+   time.sleep(0.1)
