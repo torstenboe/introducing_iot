ed5175fe88d617bf0993ab1cf6a9d688be0ef180
torstenboe <torsten.boettjer@gmail.com>
2017-04-13 14:06:10 +0200

    Correct Input Template

--- dim.py
+++ dim.py
@@ -4,14 +4,16 @@ import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT)
 
-pwm = GPIO.PWM(11, 50)
+led = GPIO.PWM(11, 100)
 pwm.start(0)
+pause = 0.1
 
-for i in range(100):
-   pwm.ChangeDutyCycle(i)
-   time.sleep(0.1)
-for i in range(100, 0, -1)
-   pwm.ChangeDutyCycle(i)
-   time.sleep(0.1)
+for i in range(0, 100 + 1):
+   led.ChangeDutyCycle(i)
+   time.sleep(pause)
+
+for i in range(100, -1, -1):
+   led.ChangeDutyCycle(i)
+   time.sleep(pause)
 
 GPIO.cleanup()
